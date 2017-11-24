d471ba80351104ab402286325f1de2f2f5764cb9
beelarr <bryonl@me.com>
2017-11-24 08:45:31 -0600

    Initial build

---
+++ index.vr.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import { View, Text, Pano, AppRegistry, asset } from 'react-vr';
+
+
+class WeatherSimulator extends Component {
+    render() {
+        return(
+            <View>
+                <Pano>
+
+                </Pano>
+            </View>
+        )
+    }
+}
